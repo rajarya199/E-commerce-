dcc9641d43a4f7edfba6e45cf82a706a39248871
rajarya199 <aryalrajan078@gmail.com>
2024-05-06 23:26:26 +0545

    home,nav

---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Slider from '../components/Slider'
+import Card from '../components/Card'
+
+const Homepage = () => {
+  return (
+    <>
+      <Slider/>
+      <Card/>
+    </>
+  )
+}
+
+export default Homepage
