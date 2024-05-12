1b22a4b25da2d0740d0724a8f4c5ce629ba59e5a
rajarya199 <aryalrajan078@gmail.com>
2024-05-12 23:06:22 +0545

    update ,delete category

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -29,3 +29,34 @@ exports.categoryDetails=async(req,res)=>{
     }
     res.send(category)
 }
+
+//to update
+exports.updateCategory=async(req,res)=>{
+    const category=await Category.findByIdAndUpdate(
+        req.params.id,{
+            category_name:req.body.category_name 
+        },
+        {new:true}
+    )
+    if(!category){
+        return res.status(400).json({error:"something went wrong"})
+    }
+    res.send(category)
+
+}
+
+//delete category
+exports.deleteCategory=async(req,res)=>{
+    Category.findByIdAndDelete(req.params.id)
+    .then (category=>{
+        if(!category){
+        return res.status(404).json({error:'category with that id not fpund'})
+        }
+        else{
+            return res.status(200).json({message:'category deleted'})
+        }
+    })
+    .catch(err=>{
+        return res.status(400).json({error:err})
+    })
+}
--- backend/routes/categoryRoute.js
+++ backend/routes/categoryRoute.js
@@ -1,8 +1,10 @@
 const express=require('express')
-const { postCategory, categoryList, categoryDetails } = require('../controllers/categoryController')
+const { postCategory, categoryList, categoryDetails, updateCategory, deleteCategory } = require('../controllers/categoryController')
 const router=express.Router()
 
 router.post('/postcategory',postCategory)
 router.get('/categorylist',categoryList)
 router.get('/categorydetails/:id',categoryDetails)
+router.put('/updatecategory/:id',updateCategory)
+router.delete('/deletecategory/:id',deleteCategory)
 module.exports=router
