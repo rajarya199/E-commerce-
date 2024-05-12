d6a161a83efff34e7417af173b0198f772b0c158
rajarya199 <aryalrajan078@gmail.com>
2024-05-12 23:13:06 +0545

    post unique category

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -6,11 +6,22 @@ exports.postCategory=async(req,res)=>{
         category_name:req.body.category_name
 
     })
-    category=await category.save()
-    if(!category){
-        return res.status(400).json({error:"something went wrong"})
-    }
-    res.send(category)
+     //to check if category already exist in db
+     Category.findOne({category_name:category.category_name})
+     .then(async categories=>{
+         if(categories){
+             return res.status(400).json({error:'category must be unique'})
+         }
+         else{
+             category= await category.save()
+             //if not save
+             if(!category){
+                 return res.status(400).json({error:'something went wrong'})
+             }
+             res.send(category)
+         }
+     })
+     .catch(err=>res.status(400).json({error:err}))
 }
 // to retrive all data
 exports.categoryList=async(req,res)=>{
