e519a6277ab4db3d3df0014b570e421a5b5e0405
rajarya199 <aryalrajan078@gmail.com>
2025-07-03 22:36:27 +0545

    header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,153 +1,121 @@
 import React from 'react'
-import { Link,useNavigate } from 'react-router-dom'
-import { isAuthenticated,signout } from '../auth'
+import { Link, useNavigate } from 'react-router-dom'
+import { isAuthenticated, signout } from '../auth'
+
 const Header = () => {
-  const navigate=useNavigate()
+  const navigate = useNavigate()
+  const user = isAuthenticated()?.user
+
   return (
     <>
-    <>
-  <div className="container-fluid">
-    <div className="row bg-secondary align-items-center">
-      <div className="col-lg-2">
-        <Link className="navbar-brand text-white" to="/">
-          Ecommerce
-        </Link>
-      </div>
-      <div className="col-lg-7">
-        <form className="d-flex">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-warning" type="submit">
-            <i className="far fa-search" />
-          </button>
-        </form>
-      </div>
-      <div className="col-lg-3">
-        <div className="d-flex">
-          <div className="offset-sm-2 col-sm-3 col-lg-3 mt-3">
-           {isAuthenticated() && isAuthenticated().user.role===1 &&
-             <Link
-             to="/admin/dashboard"
-             className="text-decoration-none text-white"
-             title="signin"
-           >Admin
-           </Link>
-           }
-             {isAuthenticated() && isAuthenticated().user.role===0 &&
-             <Link
-             to="/profile"
-             className="text-decoration-none text-white"
-             title="signin"
-           >Profile
-           </Link>
-           }
-           </div>
+      {/* Top Bar */}
+      <div className="bg-secondary py-2 text-white">
+        <div className="container d-flex flex-column flex-md-row align-items-center justify-content-between">
+          <Link className="navbar-brand text-white fw-bold fs-4" to="/">
+            Ecommerce
+          </Link>
 
-           {!isAuthenticated()&&
-           <>
-                   <div className="offset-sm-2 col-sm-3 col-lg-3">
-                <Link
-              to="/signin"
-              className="text-decoration-none text-white"
-              title="signin"
-            >
-              <i className="fas fa-sign-in-alt fs-3 my-3" />
+          {/* Search Bar */}
+          <form className="d-flex my-2 my-md-0 w-100 w-md-50">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-warning" type="submit">
+              <i className="fas fa-search" />
+            </button>
+          </form>
+
+          {/* User Actions */}
+          <div className="d-flex align-items-center mt-2 mt-md-0">
+            {/* Admin/Profile */}
+            {user?.role === 1 && (
+              <Link to="/admin/dashboard" className="text-white me-3 text-decoration-none">
+                Admin
+              </Link>
+            )}
+            {user?.role === 0 && (
+              <Link to="/profile" className="text-white me-3 text-decoration-none">
+                Profile
+              </Link>
+            )}
+
+            {/* Auth */}
+            {!user && (
+              <>
+                <Link to="/signin" className="text-white me-3" title="Signin">
+                  <i className="fas fa-sign-in-alt fs-5" />
+                </Link>
+                <Link to="/signup" className="text-white me-3" title="Signup">
+                  <i className="fas fa-user-plus fs-5" />
+                </Link>
+              </>
+            )}
+
+            {/* Logout */}
+            {user && (
+              <button
+                className="btn btn-danger btn-sm me-3"
+                onClick={() => signout(() => navigate('/signin'))}
+              >
+                Logout
+              </button>
+            )}
+
+            {/* Cart */}
+            <Link to="/cart" className="text-white position-relative">
+              <i className="fas fa-cart-plus fs-5" />
+              <span
+                className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning text-dark"
+                style={{ fontSize: 12 }}
+              >
+                5
+              </span>
             </Link>
-
-                </div>
-            <div className="col-sm-3 col-lg-3">
-            <div className="col-lg-4">
-              <Link
-                to="/signup"
-                className="text-decoration-none text-white"
-                title="signup"
-              >
-                <i className="fas fa-user-plus fs-3 my-3" />
-              </Link>
-            </div>
-          </div>
-           </>
-           }
-        {isAuthenticated() && 
-          <div className="col-sm-3 col-lg-3">
-          <div className="col-lg-4">
-          <button className='btn btn-danger mt-2 me-2 '
-                onClick={()=>signout(()=>{
-                  navigate('/signin')
-                    })}
-              >Logout</button>
-          </div>
-        </div>}
-          
-        
-          <div className="col-sm-3 col-lg-3">
-            <div className="col-lg-4">
-              <Link
-                to="/cart"
-                className="text-decoration-none text-white"
-                title="cart"
-              >
-                <i className="fas fa-cart-plus fs-3 my-3 mx-3 position-relative">
-                  <span
-                    className="position-absolute top-0 start-100 bg-warning badge rounded-pill translate-middle text-dark"
-                    style={{ fontSize: 12 }}
-                  >
-                    <span>5</span>
-                  </span>
-                </i>
-              </Link>
-            </div>
           </div>
         </div>
       </div>
-      
-    </div>
-  </div>
-  <nav className="navbar navbar-expand-lg navbar-light bg-secondary custom-nav">
-    <div className="container-fluid">
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link
-              className="nav-link active"
-              aria-current="page"
-              to="/"
-            >
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/products">
-              Products
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="#">
-              Customer Service
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-</>
 
-    
+      {/* Responsive Navigation Bar */}
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <div className="container">
+          {/* Hamburger toggler for mobile */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#mainNavbar"
+            aria-controls="mainNavbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          {/* Nav links: horizontal on md+, vertical dropdown on mobile */}
+          <div className="collapse navbar-collapse" id="mainNavbar">
+            <ul className="navbar-nav ms-auto mb-2 mb-md-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/products">
+                  Products
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  Customer Service
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
