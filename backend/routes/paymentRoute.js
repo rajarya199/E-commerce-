16c7b076ff275d653c50bba60d7ab544fe8670a0
rajarya199 <aryalrajan078@gmail.com>
2025-07-03 15:52:48 +0545

    payment controller,route

---
+++ backend/routes/paymentRoute.js
@@ -0,0 +1,7 @@
+const express=require('express')
+const { processPayment, sendStripeApi } = require('../controllers/paymentController')
+const router=express.Router()
+
+router.post('/process/payment',processPayment)
+router.get('/stripeapi',sendStripeApi)
+module.exports=router
