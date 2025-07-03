ce5c593df294e09944668d404ac0c95c4a961646
rajarya199 <aryalrajan078@gmail.com>
2025-07-03 23:54:06 +0545

    card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { IMG_URL } from '../config'
+
 const Card = (props) => {
-const {_id,product_name,product_price,product_image}=props.data
+  const { _id, product_name, product_price, product_image } = props.data
 
   return (
-    <>
-    <div className="col">
-      <div className="card">
-        {/* src=localhost:8000/ product img url */}
-        <img src={`${IMG_URL}/${product_image}`}className="card-img-top" alt={product_name}/>
-        <div className="card-body">
-          <h5 className="card-title">{product_name}</h5>
-          <h5>Rs{product_price}</h5>
-          <Link to={`/productdetails/${_id}`} className='btn btn-success'>View Details</Link>
+    <div className="w-full  p-2"> 
+      <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col h-full">
+        {/* Product Image */}
+        <img
+          src={`${IMG_URL}/${product_image}`}
+          alt={product_name}
+          className="w-full h-48 object-cover"
+        />
+        <div className="p-4 flex flex-col flex-grow">
+          <h5 className="text-lg font-semibold mb-2">{product_name}</h5>
+          <h5 className="text-gray-700 font-medium mb-4">Rs {product_price}</h5>
+          <Link
+            to={`/productdetails/${_id}`}
+            className="mt-auto inline-block bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded text-center"
+          >
+            View Details
+          </Link>
         </div>
       </div>
     </div>
-    
-    </>
   )
 }
 
--- client/src/components/CardContainer.jsx
+++ client/src/components/CardContainer.jsx
@@ -1,26 +1,27 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card'
 import { API } from '../config'
-import axios from'axios'
+import axios from 'axios'
 
 const CardContainer = () => {
-    const[products,setProducts]=useState([])
-    useEffect(()=>{
-        axios.get(`${API}/productlist`)
-        .then(res=>setProducts(res.data))
-        .catch(err=>console.log(err))
-    },[])
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`${API}/productlist`)
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
-    <>
-    <div className="container-fluid">  
-    <div className="row row-cols-1 row-cols-md-4 g-4">
-    {products && products.slice(0,8).map((item,i)=>(
-       <Card data={item} key={i}/>
-    ))}
- 
-  </div>
-  </div>
-  </>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+        {products &&
+          products.slice(0, 8).map((item, i) => (
+            <Card data={item} key={i} />
+          ))}
+      </div>
+    </div>
   )
 }
 
