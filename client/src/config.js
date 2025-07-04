96b6f928421216a5bdac58d5f58b308be9bfdfaa
rajarya199 <aryalrajan078@gmail.com>
2025-07-04 23:49:06 +0545

    config

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 //  export const API = 'http://localhost:8000/api'
 //  export const API = process.env.API_URL
 export const API = import.meta.env.VITE_API_URL;
-export const IMG_URL="http://localhost:8000"
+export const IMG_URL=import.meta.env.VITE_IMG_URL
